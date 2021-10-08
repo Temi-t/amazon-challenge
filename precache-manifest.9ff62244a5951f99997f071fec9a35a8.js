26ef9d1cb3741caab548369b8ccc2750c40ed71d
Temi-t <temiemmanuel@yahoo.com>
2021-10-08 03:34:38 +0100

    Updates

---
+++ precache-manifest.9ff62244a5951f99997f071fec9a35a8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e36f6aedaab8500d89d932a843b8b9d",
+    "url": "/amazon-challenge/index.html"
+  },
+  {
+    "revision": "3c19aa706d1247bfa790",
+    "url": "/amazon-challenge/static/css/main.8f0b020f.chunk.css"
+  },
+  {
+    "revision": "4a9ae6e80462e0bf0712",
+    "url": "/amazon-challenge/static/js/2.079a5f44.chunk.js"
+  },
+  {
+    "revision": "578dce85f2f6ab6ee58ce1b34782cc51",
+    "url": "/amazon-challenge/static/js/2.079a5f44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "332dd8bc622a95c932d0",
+    "url": "/amazon-challenge/static/js/3.6b2e9b92.chunk.js"
+  },
+  {
+    "revision": "3c19aa706d1247bfa790",
+    "url": "/amazon-challenge/static/js/main.273e0990.chunk.js"
+  },
+  {
+    "revision": "fd64731bea18657585ce",
+    "url": "/amazon-challenge/static/js/runtime-main.80ceb9f9.js"
+  },
+  {
+    "revision": "e4246ac479f53099584b0a677bf4e7c5",
+    "url": "/amazon-challenge/static/media/ad2.e4246ac4.png"
+  },
+  {
+    "revision": "1a7d775d8d218f2393fd8848a386ef6e",
+    "url": "/amazon-challenge/static/media/amazon_PNG11.1a7d775d.png"
+  },
+  {
+    "revision": "41b82b30dab30a616d1842f49b926b42",
+    "url": "/amazon-challenge/static/media/chair_banner.41b82b30.jpg"
+  },
+  {
+    "revision": "90a96657e38c078d7567fc7873fd1468",
+    "url": "/amazon-challenge/static/media/sasu-naru.90a96657.jpg"
+  }
+]);
